cda40b1b812b2955c7b21f523da3b4bb0faac7de
Szymon <strzelecszymon.3@gmail.com>
2023-03-06 20:47:23 +0100

    Added size input validation

--- src/Game/Aside.jsx
+++ src/Game/Aside.jsx
@@ -4,6 +4,7 @@ import { useState, useRef } from "react"
 const Aside = ({score, size, started, setSize, startGame, stopGame}) => {
 
     const [showCustom, setShowCustom] = useState(false)
+    const [selectedCustom, setSelectedCustom] = useState(false)
   
    
     const selectRef = useRef()
@@ -12,22 +13,41 @@ const Aside = ({score, size, started, setSize, startGame, stopGame}) => {
 
     const handleSelect = (value)=>{
       stopGame()
+      setSelectedCustom(false)
       setShowCustom(false)
       setSize([value,value])
     }
 
     const handleCustomSelect = (width, length)=>{
       stopGame()
-      //TODO VALIDATE WIDTH AND LENGHT
-      setSize([width,length])
-      setShowCustom(false)
+      if(validateSize(width,length)){
+        setSize([width,length])
+        setShowCustom(false)
+        setSelectedCustom(true)
+      }
+    }
+
+    const validateSize = (width,length)=>{
+      if(width > 0 && length > 0 && width <= 8 && length <=8){
+        if((width*length)%2==0) return true
+        else{
+          alert("Number of cells must be even")
+          return false
+        }
+      }
+      else {
+        alert("Length or width can't be bigger than 8")
+        return false;
+      }
+
     }
 
   return (
     <aside className='aside'>
         <div>Score: {score}</div>
         { !showCustom && <>
-          <select ref={selectRef} onChange={() => handleSelect(Number(selectRef.current.value))} defaultValue={String(size[0])}>
+          <select ref={selectRef} onChange={() => handleSelect(Number(selectRef.current.value))} defaultValue={selectedCustom ? 'custom' : String(size[0])}>
+          <option value='custom' hidden={!selectedCustom && 'hidden'}>Custom</option>
           <option value='2'>2 - easy</option>
           <option value='4'>4 - normal</option>
           <option value='6'>6 - very hard</option>
