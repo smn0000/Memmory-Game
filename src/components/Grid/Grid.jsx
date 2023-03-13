3f73205dfbdc53a3c0ed5574b8bcaad18b2b36d7
Szymon <strzelecszymon.3@gmail.com>
2023-03-13 15:52:08 +0100

    improvement to setting the background image

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,5 +1,38 @@
 import './styles.scss'
-import getBackgroud from './background'
+
+const svgs = [new URL('../../svgs/bat.svg',import.meta.url).href,
+    new URL('../../svgs/beetle.svg',import.meta.url).href,
+    new URL('../../svgs/butterfly.svg',import.meta.url).href,
+    new URL('../../svgs/camel.svg',import.meta.url).href,
+    new URL('../../svgs/cat.svg',import.meta.url).href,
+    new URL('../../svgs/chameleon.svg',import.meta.url).href,
+    new URL('../../svgs/cow.svg',import.meta.url).href,
+    new URL('../../svgs/crab.svg',import.meta.url).href,
+    new URL('../../svgs/crocodile.svg',import.meta.url).href,
+    new URL('../../svgs/dog.svg',import.meta.url).href,
+    new URL('../../svgs/duck.svg',import.meta.url).href,
+    new URL('../../svgs/elk.svg',import.meta.url).href,
+    new URL('../../svgs/frog.svg',import.meta.url).href,
+    new URL('../../svgs/giraffe.svg',import.meta.url).href,
+    new URL('../../svgs/hippo.svg',import.meta.url).href,
+    new URL('../../svgs/husky.svg',import.meta.url).href,
+    new URL('../../svgs/kangaroo.svg',import.meta.url).href,
+    new URL('../../svgs/lion.svg',import.meta.url).href,
+    new URL('../../svgs/mancaw.svg',import.meta.url).href,
+    new URL('../../svgs/mantee.svg',import.meta.url).href,
+    new URL('../../svgs/mianyag.svg',import.meta.url).href,
+    new URL('../../svgs/mouse.svg',import.meta.url).href,
+    new URL('../../svgs/ostrich.svg',import.meta.url).href,
+    new URL('../../svgs/owl.svg',import.meta.url).href,
+    new URL('../../svgs/penguin.svg',import.meta.url).href,
+    new URL('../../svgs/racoon.svg',import.meta.url).href,
+    new URL('../../svgs/rooster.svg',import.meta.url).href,
+    new URL('../../svgs/searay.svg',import.meta.url).href,
+    new URL('../../svgs/shark.svg',import.meta.url).href,
+    new URL('../../svgs/tiger.svg',import.meta.url).href,
+    new URL('../../svgs/toucan.svg',import.meta.url).href,
+    new URL('../../svgs/whale.svg',import.meta.url).href,
+  ]
 
 const Grid = ({grid, size , onFlip}) => {
 
@@ -8,7 +41,7 @@ const Grid = ({grid, size , onFlip}) => {
       <div className="grid" style={{gridTemplateColumns : `repeat(${size[0]},1fr)`, gridTemplateRows : `repeat(${size[1]},1fr)`}}>
         {grid.map( element => (
           <div data-guessed={element.guessed} data-flipped={element.flipped} key={element.id} className="cell" onClick={() => onFlip(element.id)}>
-              <div className="front" style={{backgroundImage: 'url('+ getBackgroud(element.image-1) +')'}}>
+              <div className="front" style={{backgroundImage: 'url('+ svgs[element.image-1] +')'}}>
               </div>
               <div className="back"></div>
           </div>
